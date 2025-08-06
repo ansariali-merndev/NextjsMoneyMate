712b3f54246766a197c3f212d4e3aa34a36c5be7
ansariali <=>
2025-08-06 20:17:06 +0530

    fix category bug

--- app/(root)/(application)/category/page.jsx
+++ app/(root)/(application)/category/page.jsx
@@ -4,9 +4,15 @@ import { CategoryCard } from "@/components/block/CategoryCard";
 import { CategoryModel } from "@/components/block/CategoryModel";
 import { Title } from "@/components/block/Title";
 import { useUserContext } from "@/context/UserContext";
+import { useEffect, useState } from "react";
 
 export default function Category() {
   const { categories } = useUserContext();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(categories);
+  }, [categories]);
 
   return (
     <section>
@@ -15,7 +21,7 @@ export default function Category() {
         <CategoryModel />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {categories.map((item, index) => (
+        {data.map((item, index) => (
           <CategoryCard item={item} key={index} />
         ))}
       </div>
